b804f1532caa5e18cb7f7e4f3e10bc753bcb14e2
mayank558 <61516165+mayank558@users.noreply.github.com>
2021-05-16 23:05:31 +0530

    Modified post request to chk on category thanks kapil

--- routes/product.js
+++ routes/product.js
@@ -3,72 +3,81 @@ const router = express.Router();
 const Product = require("../models/productSchema");
 const createError = require("http-errors");
 const mongoose = require("mongoose");
+const Category = require("../models/categorySchema");
 
 // Get all products without description for a specific category from database and send to user
 router.get("/:category_name", (req, res, next) => {
 
-  Product.find({ categoryName: req.params.category_name }, {description: 0})
-    .then((data) => {
-      // ! ? if category_name is not present in database than should i return error or simpli return []
-      // kapil -> I think u should give error at this point coz a user cannot call that api anyway and if somehow we are calling that api point then it would be better if we got the error for easy debugging the problem
-      if (!data || !data.length) {
-        throw createError(404, "Category does not exits");
-      }
-      res.json(data);
-    })
-    .catch((err) => {
-      next(err);
-      res.json({ message: err });
-    });
+    Product.find({ categoryName: req.params.category_name }, { description: 0 })
+        .then((data) => {
+            // ! ? if category_name is not present in database than should i return error or simpli return []
+            // kapil -> I think u should give error at this point coz a user cannot call that api anyway and if somehow we are calling that api point then it would be better if we got the error for easy debugging the problem
+            if (!data || !data.length) {
+                throw createError(404, "Category does not exits");
+            }
+            res.json(data);
+        })
+        .catch((err) => {
+            next(err);
+            res.json({ message: err });
+        });
 });
 
 //Get unique product from database and send to user
 router.get("/product/:product_id", (req, res, next) => {
 
-  Product.find({_id : req.params.product_id})
-    .then((data) => {
-      if (!data || !data.length) {
-        throw createError(404, "Product does not exits");
-      }
-      res.json(data);
-    })
-    .catch((err) => {
-      if (err instanceof mongoose.CastError) {
-        next(createError(400, "Invalid product id"));
-        return;
-      }
-      next(err);
-    });
+    Product.find({ _id: req.params.product_id })
+        .then((data) => {
+            if (!data || !data.length) {
+                throw createError(404, "Product does not exits");
+            }
+            res.json(data);
+        })
+        .catch((err) => {
+            if (err instanceof mongoose.CastError) {
+                next(createError(400, "Invalid product id"));
+                return;
+            }
+            next(err);
+        });
 });
 
 // Post a product to database
 router.post("/", (req, res, next) => {
 
-  const product = new Product({
-    categoryId: req.body.categoryId,
-    categoryName: req.body.categoryName,
-    productName: req.body.productName,
-    brandName: req.body.brandName,
-    productName: req.body.productName,
-    price: req.body.price,
-    description: req.body.description,
-    is_3dmodel: req.body.is_3dmodel,
-    productImage: req.body.productImage,
-  });
-
-  product
-    .save()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        next(createError(422, err.message));
-        return;
-      }
-      next(err);
+    const product = new Product({
+        categoryId: req.body.categoryId,
+        categoryName: req.body.categoryName,
+        productName: req.body.productName,
+        brandName: req.body.brandName,
+        productName: req.body.productName,
+        price: req.body.price,
+        description: req.body.description,
+        is_3dmodel: req.body.is_3dmodel,
+        productImage: req.body.productImage,
     });
-    
+    Category.find({ categoryName: req.body.categoryName }).then((data) => {
+            if (!data || !data.length) {
+                throw createError(404, "Make Sure to spell Category Name Correect and ID");
+            } else {
+                product
+                    .save()
+                    .then((data) => {
+                        res.json(data);
+                    })
+                    .catch((err) => {
+                        if (err.name === "ValidationError") {
+                            next(createError(422, err.message));
+                            return;
+                        }
+                        next(err);
+                    });
+            }
+        })
+        .catch((err) => {
+            next(err);
+            res.json({ message: err });
+        });
 });
 
 module.exports = router;
