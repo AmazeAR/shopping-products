5c809408c5a1aceffb96509dd4a21e1cdb8f6784
KAPIL <kapil992000@gmail.com>
2021-05-26 15:12:34 +0530

    both env and app.js file are out out of src folder

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const createError = require("http-errors");
 const morgan = require("morgan");
-const sendResponse = require('./lib/response');
+const sendResponse = require('./src/lib/response');
 
 require("dotenv").config();
 
@@ -15,26 +15,26 @@ app.use(cors());
 // app.use(morgan("dev"));
 
 // Import Routes
-const categoryRoute = require("./routes/category");
+const categoryRoute = require("./src/routes/category");
 app.use("/categories", categoryRoute);
 
-const productsRoute = require("./routes/product");
+const productsRoute = require("./src/routes/product");
 app.use("/products", productsRoute);
 
-const usersRoute = require("./routes/user");
+const usersRoute = require("./src/routes/user");
 app.use("/users", usersRoute);
 
-const personalCartRoute = require("./routes/personalCart");
+const personalCartRoute = require("./src/routes/personalCart");
 app.use("/personalCart", personalCartRoute);
 
-const groupCartRoute = require("./routes/groupCart");
+const groupCartRoute = require("./src/routes/groupCart");
 app.use("/groupCart", groupCartRoute);
 
-const modelRoute = require("./routes/model");
+const modelRoute = require("./src/routes/model");
 app.use("/model", modelRoute);
 
 // connect to mongoDB
-require("./initDB")();
+require("./src/initDB")();
 
 //base url
 app.get("/", (req, res) => {
