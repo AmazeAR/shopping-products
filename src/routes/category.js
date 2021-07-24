94462768fa518c33fe557b62d64d5b14f14e1508
KAPIL <kapil992000@gmail.com>
2021-07-24 17:03:10 +0530

    minor changes

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,43 +2,45 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../models/categorySchema");
 const createError = require("http-errors");
-const sendResponse = require('../lib/response');
+const sendResponse = require("../lib/response");
 
+const mongo = require("../../initDB");
 
 router.get("/", (req, res, next) => {
-  
-  Category.find({})
-    .then((data) => {
-      if (!data || !data.length) {
-        throw createError(404, "Categories not found!");
-      }
-      sendResponse({response: res, data: data, error: null});
-    })
-    .catch((err) => {
-      next(err);
-      sendResponse({response: res, data: null, error: err});
-    });
+	mongo().then(() => {
+		Category.find({})
+			.then((data) => {
+				if (!data || !data.length) {
+					throw createError(404, "Categories not found!");
+				}
+				sendResponse({ response: res, data: data, error: null });
+			})
+			.catch((err) => {
+				next(err);
+				sendResponse({ response: res, data: null, error: err });
+			});
+	});
 });
 
 router.post("/", (req, res, next) => {
+	const category = new Category({
+		categoryName: req.body.categoryName,
+		categoryImage: req.body.categoryImage,
+	});
 
-  const category = new Category({
-    categoryName: req.body.categoryName,
-    categoryImage: req.body.categoryImage,
-  });
-
-  category.save()
-    .then((data) => {
-      sendResponse({response: res, data: data, error: null});
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        next(createError(422, err.message));
-        return;
-      }
-      next(err);
-      sendResponse({response: res, data: null, error: err});
-    });
+	category
+		.save()
+		.then((data) => {
+			sendResponse({ response: res, data: data, error: null });
+		})
+		.catch((err) => {
+			if (err.name === "ValidationError") {
+				next(createError(422, err.message));
+				return;
+			}
+			next(err);
+			sendResponse({ response: res, data: null, error: err });
+		});
 });
 
 module.exports = router;
